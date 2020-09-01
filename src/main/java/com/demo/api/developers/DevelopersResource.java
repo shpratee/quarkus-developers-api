0a63b4bae0c1ee03f2e327a54a7801aa0b19a533
shpratee <prateek.xa.sharma@gmail.com>
2020-09-01 10:39:18 +0100

    Adding content for DI and logging configuration

--- src/main/java/com/demo/api/developers/DevelopersResource.java
+++ src/main/java/com/demo/api/developers/DevelopersResource.java
@@ -1,8 +1,10 @@
 package com.demo.api.developers;
 
 import com.demo.api.developers.model.Developer;
-import com.demo.api.developers.model.Skill;
 import com.demo.api.developers.service.DeveloperService;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -13,8 +15,21 @@ import javax.ws.rs.core.UriBuilder;
 @Path("/developers")
 public class DevelopersResource {
 
+    private static Logger logger = LoggerFactory.getLogger(DevelopersResource.class);
+
     @Inject
     DeveloperService service;
+
+    @ConfigProperty(name="developer.greeting.message")
+    String message;
+
+    @GET
+    @Path("/hello")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello(){
+        logger.debug("In hello() method");
+        return message;
+    }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
